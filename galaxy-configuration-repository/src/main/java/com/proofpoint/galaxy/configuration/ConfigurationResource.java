599d9d22951134d29a13bb5f9bb11bafd9c93d5b
Dain Sundstrom <dain@iq80.com>
2011-09-16 13:54:21 -0700

    Automatically add node.environment and discovery.uri to etc/config.property files

--- galaxy-configuration-repository/src/main/java/com/proofpoint/galaxy/configuration/ConfigurationResource.java
+++ galaxy-configuration-repository/src/main/java/com/proofpoint/galaxy/configuration/ConfigurationResource.java
@@ -1,7 +1,12 @@
 package com.proofpoint.galaxy.configuration;
 
+import com.google.common.base.Charsets;
+import com.google.common.base.Joiner;
+import com.google.common.io.ByteStreams;
+import com.google.common.io.CharStreams;
 import com.google.common.io.InputSupplier;
 import com.proofpoint.galaxy.shared.ConfigSpec;
+import com.proofpoint.node.NodeInfo;
 
 import javax.inject.Inject;
 import javax.ws.rs.GET;
@@ -11,17 +16,20 @@ import javax.ws.rs.core.Response;
 import javax.ws.rs.core.Response.Status;
 import java.io.InputStream;
 import java.net.URI;
+import java.util.List;
 import java.util.Map;
 
 @Path("/v1/config/{environment}/{type}/{pool}/{version}")
 public class ConfigurationResource
 {
     private final ConfigurationRepository configurationRepository;
+    private final DiscoverDiscovery discoverDiscovery;
 
     @Inject
-    public ConfigurationResource(ConfigurationRepository configRepository)
+    public ConfigurationResource(ConfigurationRepository configRepository, DiscoverDiscovery discoverDiscovery)
     {
         this.configurationRepository = configRepository;
+        this.discoverDiscovery = discoverDiscovery;
     }
 
     @GET
@@ -52,6 +60,21 @@ public class ConfigurationResource
         InputSupplier<? extends InputStream> configFile = configurationRepository.getConfigFile(configSpec, path);
         if (configFile == null) {
             return Response.status(Status.NOT_FOUND).build();
+        }
+
+        // add discovery url to config.properties
+        if (path.equals("etc/config.properties")) {
+            StringBuilder additionalConfig = new StringBuilder();
+            additionalConfig.append("#\n");
+            additionalConfig.append("# Automatically provided by Galaxy Configuration Repository\n");
+            additionalConfig.append("node.environment=").append(environment).append("\n");
+            List<URI> discoveryServers = discoverDiscovery.getDiscoveryServers();
+            if (!discoveryServers.isEmpty()) {
+                additionalConfig.append("discovery.uri=").append(Joiner.on(",").join(discoveryServers)).append("\n");
+            }
+            additionalConfig.append("\n");
+
+            configFile = ByteStreams.join(configFile, ByteStreams.newInputStreamSupplier(additionalConfig.toString().getBytes(Charsets.UTF_8)));
         }
 
         return Response.ok(new InputSupplierStreamingOutput(configFile)).build();
