fcd65f92e7d175b457710fc0092eba02d55487f5
Dain Sundstrom <dain@iq80.com>
2011-10-07 13:23:21 -0700

    AwsProvision is now the default

--- galaxy-coordinator/src/main/java/com/proofpoint/galaxy/coordinator/CoordinatorMain.java
+++ galaxy-coordinator/src/main/java/com/proofpoint/galaxy/coordinator/CoordinatorMain.java
@@ -34,7 +34,7 @@ public class CoordinatorMain
                 new MBeanModule(),
                 new JmxModule(),
                 new CoordinatorMainModule(),
-                new LocalProvisionerModule());
+                new AwsProvisionerModule());
 
         app.strictConfig().initialize();
     }
