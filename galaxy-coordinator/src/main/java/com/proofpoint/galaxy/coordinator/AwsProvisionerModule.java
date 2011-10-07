d1e26d55e30d50cba2cc3b6e989304d0e1bffb02
Dain Sundstrom <dain@iq80.com>
2011-10-07 14:35:43 -0700

    Bind correct provisioner in aws module

--- galaxy-coordinator/src/main/java/com/proofpoint/galaxy/coordinator/AwsProvisionerModule.java
+++ galaxy-coordinator/src/main/java/com/proofpoint/galaxy/coordinator/AwsProvisionerModule.java
@@ -31,7 +31,7 @@ public class AwsProvisionerModule
         binder.disableCircularProxies();
         binder.requireExplicitBindings();
 
-        binder.bind(Provisioner.class).to(LocalProvisioner.class).in(Scopes.SINGLETON);
+        binder.bind(Provisioner.class).to(AwsProvisioner.class).in(Scopes.SINGLETON);
         ConfigurationModule.bindConfig(binder).to(AwsProvisionerConfig.class);
     }
 
