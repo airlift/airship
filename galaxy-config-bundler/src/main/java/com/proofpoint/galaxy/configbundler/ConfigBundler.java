9422dd2f01a324d04342dc9f195acca6b62fefd1
Martin Traverso <mtraverso@acm.org>
2012-04-03 15:11:28 -0700

    suppress logging messages

--- galaxy-config-bundler/src/main/java/com/proofpoint/galaxy/configbundler/ConfigBundler.java
+++ galaxy-config-bundler/src/main/java/com/proofpoint/galaxy/configbundler/ConfigBundler.java
@@ -1,8 +1,13 @@
 package com.proofpoint.galaxy.configbundler;
 
+import com.google.common.io.NullOutputStream;
+import com.proofpoint.log.Logging;
+import com.proofpoint.log.LoggingConfiguration;
 import org.iq80.cli.Cli;
 import org.iq80.cli.Help;
 
+import java.io.IOException;
+import java.io.PrintStream;
 import java.util.concurrent.Callable;
 
 import static org.iq80.cli.Cli.buildCli;
@@ -12,6 +17,8 @@ public class ConfigBundler
     public static void main(String[] args)
             throws Exception
     {
+        initializeLogging(false);
+
         Cli<Callable> cli = buildCli("configgy", Callable.class)
                 .withDefaultCommand(Help.class)
                 .withCommands(ReleaseCommand.class,
@@ -24,4 +31,29 @@ public class ConfigBundler
         cli.parse(args).call();
     }
 
+    public static void initializeLogging(boolean debug)
+            throws IOException
+    {
+        // unhook out and err while initializing logging or logger will print to them
+        PrintStream out = System.out;
+        PrintStream err = System.err;
+        try {
+            if (debug) {
+                Logging logging = new Logging();
+                logging.initialize(new LoggingConfiguration());
+            }
+            else {
+                System.setOut(new PrintStream(new NullOutputStream()));
+                System.setErr(new PrintStream(new NullOutputStream()));
+
+                Logging logging = new Logging();
+                logging.initialize(new LoggingConfiguration());
+                logging.disableConsole();
+            }
+        }
+        finally {
+            System.setOut(out);
+            System.setErr(err);
+        }
+    }
 }
