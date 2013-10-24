32bacb79214c5b22b4d2fa4ff6c5a16eab3b0dfd
Jason van Zyl <jason@tesla.io>
2013-10-24 15:22:54 -0700

    Extract command classes into separate project

--- airship-shared/src/main/java/io/airlift/airship/shared/CommandTimeoutException.java
+++
@@ -1,9 +0,0 @@
-package io.airlift.airship.shared;
-
-public class CommandTimeoutException extends CommandFailedException
-{
-    public CommandTimeoutException(Command command)
-    {
-        super(command, "did not complete in " + command.getTimeLimit(), null);
-    }
-}
